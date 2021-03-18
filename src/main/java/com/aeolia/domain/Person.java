9997275ccf155fb8600d37a49db8f413b5f4a6a4
yaojinxin <18072977615@163.com>
2021-03-18 20:13:45 +0800

    创建Person类

---
+++ src/main/java/com/aeolia/domain/Person.java
@@ -0,0 +1,79 @@
+package com.aeolia.domain;
+
+import java.time.LocalDateTime;
+
+/**
+ * @ClassName Person
+ * @Description: 人pojo类
+ * @Author 姚金鑫
+ * @CreateDate 2021/3/18 20:10
+ * @Version 1.0
+ */
+public class Person {
+    private Integer id;
+    private String name;
+    private String sex;
+    private Integer age;
+    private String address;
+    private LocalDateTime birthday;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", birdhday=" + birthday +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDateTime birthday) {
+        this.birthday = birthday;
+    }
+}
